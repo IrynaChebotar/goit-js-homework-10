0b28f72737b9729b2cc5c295fb0dfbacca760bbc
IrynaChebotar <iryna.chebotar2@gmail.com>
2023-08-04 11:52:41 +0200

    img

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,61 +1,61 @@
+
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
 axios.defaults.headers.common['x-api-key'] =
 'live_UvmRvPcq5CNoEMbMbTzJaJnvlWGQC1r397OnZ0EsOzyNJfomdvBQju0VHjI59rTE';
 
-export function fetchBreeds() {
-Notiflix.Loading.pulse('Loading data, please wait', {
-backgroundColor: 'rgba(0,0,0,0.8)',
-});
+export function fetchCatBreeds() {
+  Notiflix.Loading.pulse('Loading data, please wait', {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  });
 
-return axios
-.get('https://api.thecatapi.com/v1/breeds')
-.then(response => {
-const breeds = response.data;
-Notiflix.Loading.remove();
-return breeds;
-})
-.catch(error => {
-handleError();
-Notiflix.Loading.remove();
-throw error;
-});
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      const breeds = response.data;
+      Notiflix.Loading.remove();
+      return breeds;
+    })
+    .catch(error => {
+      handleFetchError();
+      Notiflix.Loading.remove();
+      throw error;
+    });
 }
 
-export function fetchCatByBreed(breedId) {
-Notiflix.Loading.pulse('Loading data, please wait', {
-backgroundColor: 'rgba(0,0,0,0.8)',
-});
+export function fetchCatByBreedId(breedId) {
+  Notiflix.Loading.pulse('Loading data, please wait', {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  });
 
-return axios
-.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-.then(response => {
-const cat = response.data[0];
-const breed = cat.breeds[0];
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      const cat = response.data[0];
+      const breed = cat.breeds[0];
 
-const catData = {
-catUrl: cat.url,
-breedName: breed.name,
-description: breed.description,
-temperament: breed.temperament,
-};
+      const catData = {
+        imageUrl: cat.url,
+        breedName: breed.name,
+        description: breed.description,
+        temperament: breed.temperament,
+      };
 
-Notiflix.Loading.remove();
-return catData;
-})
-.catch(error => {
-Notiflix.Loading.remove();
-return { error: true };
-// throw error;
-});
+      Notiflix.Loading.remove();
+      return catData;
+    })
+    .catch(error => {
+      Notiflix.Loading.remove();
+      return { error: true };
+    });
 }
 
-function handleError() {
-const error = document.querySelector('.error');
-error.style.display = 'block';
-error.style.color = 'red';
-Notiflix.Notify.failure(
-'Oops! Something went wrong! Try reloading the page!'
-);
+function handleFetchError() {
+  const errorElement = document.querySelector('.error');
+  errorElement.style.display = 'block';
+  errorElement.style.color = 'red';
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,85 +1,83 @@
+
 import SlimSelect from 'slim-select';
-import Notiflix from 'notiflix';
 import Swal from 'sweetalert2';
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { fetchCatBreeds, fetchCatByBreedId } from './cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
-const error = document.querySelector('.error');
-const catInfo = document.querySelector('.cat-info');
+const errorElement = document.querySelector('.error');
 
-function renderBreeds(breeds) {
-breeds.forEach(breed => {
-const option = document.createElement('option');
-option.value = breed.id;
-option.textContent = breed.name;
-breedSelect.appendChild(option);
-});
+function displayCatBreeds(breeds) {
+  breeds.forEach(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  });
 
-new SlimSelect({
-select: '#single',
-});
+  new SlimSelect({
+    select: '#single',
+  });
 
-breedSelect.disabled = false;
-error.style.display = 'none';
+  breedSelect.disabled = false;
+  errorElement.style.display = 'none';
 }
 
-function renderCat(catData) {
-const catImage = document.createElement('img');
-catImage.src = catData.catUrl;
-catImage.alt = catData.breedName;
-catImage.classList.add('cat-image');
+function displayCatInfo(catData) {
+  const catImage = document.createElement('img');
+  catImage.src = catData.imageUrl;
+  catImage.alt = catData.breedName;
+  catImage.classList.add('cat-image');
 
-const breedName = document.createElement('h2');
-breedName.textContent = catData.breedName;
+  const breedName = document.createElement('h2');
+  breedName.textContent = catData.breedName;
 
-const description = document.createElement('p');
-description.textContent = catData.description;
+  const description = document.createElement('p');
+  description.textContent = catData.description;
 
-const temperament = document.createElement('p');
-temperament.textContent = catData.temperament;
+  const temperament = document.createElement('p');
+  temperament.textContent = catData.temperament;
 
-const modalContent = document.createElement('div');
-modalContent.appendChild(catImage);
-modalContent.appendChild(breedName);
-modalContent.appendChild(description);
-modalContent.appendChild(temperament);
+  const modalContent = document.createElement('div');
+  modalContent.appendChild(catImage);
+  modalContent.appendChild(breedName);
+  modalContent.appendChild(description);
+  modalContent.appendChild(temperament);
 
-Swal.fire({
-title: 'Cat Information',
-html: modalContent.innerHTML,
-showCloseButton: true,
-showConfirmButton: false,
-animation: true,
-showClass: {
-popup: 'animate__animated animate__fadeInDown',
-},
-hideClass: {
-popup: 'animate__animated animate__fadeOutUp',
-},
-});
+  Swal.fire({
+    title: 'Cat Information',
+    html: modalContent.innerHTML,
+    showCloseButton: true,
+    showConfirmButton: false,
+    animation: true,
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown',
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp',
+    },
+  });
 }
 
 function handleBreedSelectChange() {
-const selectedBreedId = breedSelect.value;
-fetchCatByBreed(selectedBreedId)
-.then(catData => {
-renderCat(catData);
-})
-.catch(error => {
-console.error(error);
-});
+  const selectedBreedId = breedSelect.value;
+  fetchCatByBreedId(selectedBreedId)
+    .then(catData => {
+      displayCatInfo(catData);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
 breedSelect.addEventListener('change', handleBreedSelectChange);
 
 async function initializeApp() {
-try {
-await fetchBreeds().then(breeds => {
-renderBreeds(breeds);
-});
-} catch (error) {
-console.error(error);
-}
+  try {
+    const breeds = await fetchCatBreeds();
+    displayCatBreeds(breeds);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 initializeApp();
